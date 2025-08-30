1b119cb7a33625117413c4365c293ab1a6471cc4
Daniel <daniel.andressanchez12@gmail.com>
2025-08-30 23:27:08 +0000

    2

--- proga1/Figuras.java
+++ proga1/Figuras.java
@@ -1,81 +1,65 @@
-public class Figuras 
-{
-  public void imprimirFigura1(int n)
-    {
-        
-        for (int i = 0; i < n; i++) 
-        {
-            
-            for (int j = 0; j < n; j++) 
+public class Figuras {
+    public void imprimirFigura1(int n) {
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++)
                 System.out.print("*");
-            
+
             System.out.println();
-            
+
         }
     }
-    
 
-    public void imprimirFigura2(int n) 
-    {
-        
-        for (int i = 1; i <= n; i++) 
-        {
-            
-            for (int j = 0; j < i; j++) 
+    public void imprimirFigura2(int n) {
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 0; j < i; j++)
                 System.out.print("*");
-            
+
             System.out.println();
-            
+
         }
     }
-    
 
-    public void imprimirFigura3(int n) 
-    {
-        
-        for (int i = n; i > 0; i--) 
-        {
-            
-            for (int j = 0; j < i; j++) 
+    public void imprimirFigura3(int n) {
+
+        for (int i = n; i > 0; i--) {
+
+            for (int j = 0; j < i; j++)
                 System.out.print("*");
-            
+
             System.out.println();
-            
+
         }
     }
-    
 
-    public void imprimirFigura4(int n) 
-    {
-        for (int i = 1; i <= n; i++) 
-        {
- 
-            for (int j = 0; j < n - i; j++) 
+    public void imprimirFigura4(int n) {
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 0; j < n - i; j++)
                 System.out.print(" ");
 
-            for (int j = 0; j < i; j++) 
+            for (int j = 0; j < i; j++)
                 System.out.print("*");
-            
             System.out.println();
+
         }
+
     }
-    
 
-    public void imprimirFigura5(int n) 
-    {
-        for (int i = n; i > 0; i--) 
-        {
+    public void imprimirFigura5(int n) {
+        for (int i = n; i > 0; i--) {
 
-            for (int j = 0; j < n - i; j++) 
+            for (int j = 0; j < n - i; j++)
                 System.out.print(" ");
 
-            for (int j = 0; j < i; j++) 
+            for (int j = 0; j < i; j++)
                 System.out.print("*");
-            
+
             System.out.println();
         }
     }
 
-    
 }
-
